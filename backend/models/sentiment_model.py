72baf5c9fbe5f407d7595e0f5335ee434aa25476
Sejalchoudhari40-lang <sejalchoudhari40@gmail.com>
2026-07-02 11:31:35 +0530

    Update sentiment_model.py

--- backend/models/sentiment_model.py
+++ backend/models/sentiment_model.py
@@ -1,15 +1,23 @@
 from transformers import pipeline
 
-# Load HuggingFace Sentiment Model
-sentiment_pipeline = pipeline(
-    "sentiment-analysis",
-    model="distilbert-base-uncased-finetuned-sst-2-english"
-)
+_sentiment = None
+
+def load_pipeline():
+    global _sentiment
+
+    if _sentiment is None:
+        _sentiment = pipeline(
+            "sentiment-analysis",
+            model="distilbert-base-uncased-finetuned-sst-2-english"
+        )
+
+    return _sentiment
 
 
 def analyze_sentiment(text: str):
+    sentiment = load_pipeline()
 
-    result = sentiment_pipeline(text)[0]
+    result = sentiment(text)[0]
 
     return {
         "label": result["label"],
